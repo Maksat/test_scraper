b4fbb04a27c79e7a6dc3e8f7472a5c1c781c55a5
Maksat <amangeldiyev.m@gmail.com>
2018-06-22 18:07:51 +0800

    Add files via upload

--- scraper.js
+++ scraper.js
@@ -267,7 +267,7 @@ var q = tress(function(url, callback){
 
          callback(url);
     });
-}, 1); // запускаем 10 параллельных потоков
+}, 10); // запускаем 10 параллельных потоков
 
 q.drain = function(){
 	fs.appendFileSync('./data.json', results.join("\n"));//JSON.stringify(results, null, 4));
